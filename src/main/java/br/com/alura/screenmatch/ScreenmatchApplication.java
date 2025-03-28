c31f7786919eb87b1b4d4f536a9c2b2ad0ee62ba
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-03-28 08:48:01 -0300

    Init project with DadosSerie record, ConsumoApi class, ConverteDados class and IConverteDados interface, using jackson-databind

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,31 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URI;
+import java.net.http.HttpRequest;
+
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+//		var json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+//		System.out.println(json);
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=rick+and+morty&apikey=8889803a");
+		System.out.println(json);
+		var converteDados = new ConverteDados();
+		var dadosSerie = converteDados.converterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie.toString());
+	}
 }
